bcea8c4754fc7043b2ab33fc60a7053aa549546f
John Kang <john@tosslab.com>
2017-07-24 10:51:42 +0900

    feat(packer@1.0.2.rb): add packer@1.0.2

---
+++ packer@1.0.2.rb
@@ -0,0 +1,91 @@
+require "language/go"
+
+class PackerAT102 < Formula
+  desc "Tool for creating identical machine images for multiple platforms"
+  homepage "https://packer.io"
+  url "https://github.com/hashicorp/packer.git",
+      :tag => "v1.0.2",
+      :revision => "6a8ae4b258d1bb0beece05c9fc3bbc1c186a0480"
+  head "https://github.com/hashicorp/packer.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6b7dd6df7d2020553dba03db75f6c86395fa9fe602b92225447a8f0a3edc54d9" => :sierra
+    sha256 "af1f5afce107799415e68f4f0ab45201bb3527e9b14bc1cec14df3708679cc76" => :el_capitan
+    sha256 "71c62e8d113432c28072b106ed29db91d2510c35a5dd240c702565f54d53393d" => :yosemite
+  end
+
+  depends_on "go" => :build
+  depends_on "govendor" => :build
+
+  go_resource "github.com/mitchellh/gox" do
+    url "https://github.com/mitchellh/gox.git",
+        :revision => "c9740af9c6574448fd48eb30a71f964014c7a837"
+  end
+
+  go_resource "github.com/mitchellh/iochan" do
+    url "https://github.com/mitchellh/iochan.git",
+        :revision => "87b45ffd0e9581375c491fef3d32130bb15c5bd7"
+  end
+
+  def install
+    ENV["XC_OS"] = "darwin"
+    ENV["XC_ARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
+    ENV["GOPATH"] = buildpath
+    # For the gox buildtool used by packer, which
+    # doesn't need to be installed permanently.
+    ENV.append_path "PATH", buildpath
+
+    packerpath = buildpath/"src/github.com/hashicorp/packer"
+    packerpath.install Dir["{*,.git}"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/mitchellh/gox" do
+      system "go", "build"
+      buildpath.install "gox"
+    end
+
+    cd packerpath do
+      # We handle this step above. Don't repeat it.
+      inreplace "Makefile" do |s|
+        s.gsub! "go get github.com/mitchellh/gox", ""
+        s.gsub! "go get golang.org/x/tools/cmd/stringer", ""
+        s.gsub! "go get github.com/kardianos/govendor", ""
+      end
+
+      (buildpath/"bin").mkpath
+      if build.head?
+        system "make", "bin"
+      else
+        system "make", "releasebin"
+      end
+      bin.install buildpath/"bin/packer"
+      zsh_completion.install "contrib/zsh-completion/_packer"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    minimal = testpath/"minimal.json"
+    minimal.write <<-EOS.undent
+      {
+        "builders": [{
+          "type": "amazon-ebs",
+          "region": "us-east-1",
+          "source_ami": "ami-59a4a230",
+          "instance_type": "m3.medium",
+          "ssh_username": "ubuntu",
+          "ami_name": "homebrew packer test  {{timestamp}}"
+        }],
+        "provisioners": [{
+          "type": "shell",
+          "inline": [
+            "sleep 30",
+            "sudo apt-get update"
+          ]
+        }]
+      }
+    EOS
+    system "#{bin}/packer", "validate", minimal
+  end
+end
